65f10f0e81b0b737a2905c5713ff65b87267b676
transtone <zm3345@126.com>
2017-05-03 11:28:52 +0800

    update

--- src/TrixVue.vue
+++ src/TrixVue.vue
@@ -1,40 +1,87 @@
 <template>
   <div class="trixWrapper">
     <div ref="trixContainer" :id="id">
-      <trix-editor :input="inputId"></trix-editor>
+      <trix-editor input="x" @trix-change="handleTyping" @trix-attachment-add="handleUpload"></trix-editor>
+      <input type="hidden" :value="content" id="x">
     </div>
   </div>
 </template>
 
 <script>
-  import 'trix';
-  import 'trix/dist/trix.css';
+  import '../../../static/trix/trix'
+  import '../../../static/trix/trix.css'
 
   export default {
     name: 'trix-vue',
-
     props: {
       id: {
         type: String,
         default: 'trix-container'
       },
-      inputId: {
+      content: {
         type: String,
         default: ''
       }
     },
-
     data() {
       return {
+        host: "https://d13txem1unpe48.cloudfront.net/"
       }
     },
-
     methods: {
+      handleTyping(event) {
+        const element = event.target
+        const html = element.value
+        this.$emit('update:content', html)
+      },
+      handleUpload(event) {
+        console.log(event)
+      },
+      uploadAttachment(attachment) {
+        var file, form, key, xhr;
+        file = attachment.file;
+        key = this.createStorageKey(file);
+        form = new FormData;
+        form.append("key", key);
+        form.append("Content-Type", file.type);
+        form.append("file", file);
+        xhr = new XMLHttpRequest;
+        xhr.open("POST", this.host, true);
+        xhr.upload.onprogress = function (event) {
+          var progress;
+          progress = event.loaded / event.total * 100;
+          return attachment.setUploadProgress(progress);
+        };
+        xhr.onload = function () {
+          var href, url;
+          if (xhr.status === 204) {
+            url = href = this.host + key;
+            return attachment.setAttributes({
+              url: url,
+              href: href
+            });
+          }
+        };
+        return xhr.send(form);
+      },
+      createStorageKey(file) {
+        var date, day, time;
+        date = new Date();
+        day = date.toISOString().slice(0, 10);
+        time = date.getTime();
+        return "tmp/" + day + "/" + time + "-" + file.name;
+      }
+    },
+    mounted() {
+      // document.addEventListener("trix-attachment-add", function (event) {
+      //   var attachment
+      //   attachment = event.attachment
+      //   if (attachment.file) {
+      //     return this.uploadAttachment(attachment)
+      //   }
+      // }.bind(this))
+
     }
   }
-</script>
 
-<style>
-  #trix-container {
-  }
-</style>
+</script>
